f087a33388f562da97ae7d7b1dbf72b3b8285dde
Yanya Zanevskaya <jstcandid@MacBook-Air-Yanya.local>
2021-02-15 16:25:00 +0300

    Init

---
+++ firstProject/js/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+//alert("Hello");
+
+// const result =confirm("Are yo here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?","yep");
+// console.log(typeof(answer));
+
+ const answers = [];
+
+ answers[0] = prompt("Whats your name","");
+ answers[1] = prompt("Whats your thirname","");
+ answers[2] = prompt("Whats your age","");
+
+ document.write(answers);
